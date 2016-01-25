4f305758f885a834012bbee87623a96ef24694b1
cvcpp <cdvigus@cpp.edu>
2016-01-25 13:01:39 -0800

    Displays json from static api call

--- web/controllers/apitest.ex
+++ web/controllers/apitest.ex
@@ -1,7 +1,13 @@
 defmodule KWYE.Apitest do
   use KWYE.Web, :controller
+  
+  HTTPoison.start
+
+
+  @api_key Application.get_env( :kwye, :ndb_key )
+
 
   def index(conn, _params) do
-    render conn, "index.html"
+    text conn, (HTTPoison.get! "http://api.nal.usda.gov/ndb/reports/?ndbno=01009&type=b&format=json&api_key=" <> @api_key).body
   end
 end
