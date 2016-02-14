a5c9b62f360fa3ca3d7a79274dfb4f37dcec5c7e
cvcpp <cdvigus@cpp.edu>
2016-02-14 04:00:09 -0800

    Added test for my assignment3

---
+++ test/controllers/assignment3_test.exs
@@ -0,0 +1,8 @@
+defmodule KWYE.Assignment3Test do
+  use KWYE.ConnCase
+
+  test "GET /chris", %{conn: conn} do
+    conn = get conn, "/chris"
+    assert html_response(conn, 200) =~ "chris hello"
+  end
+end
