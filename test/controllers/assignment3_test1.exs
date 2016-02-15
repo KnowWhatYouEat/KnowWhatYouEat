392987516fa54f89f04973e55ddfbd85e9ad17e1
kalyan7 <kalyan7.boppana@gmail.com>
2016-02-14 22:29:46 -0800

    test case

--- test/controllers/assignment3_test1.exs
+++ test/controllers/assignment3_test1.exs
@@ -2,7 +2,7 @@ defmodule KWYE.Assignment3Test do
   use KWYE.ConnCase
 
   test "GET /kalyan", %{conn: conn} do
-    conn = get conn, "/chris"
+    conn = get conn, "/kalyan"
     assert html_response(conn, 200) =~ "kalyan hello"
   end
 end
