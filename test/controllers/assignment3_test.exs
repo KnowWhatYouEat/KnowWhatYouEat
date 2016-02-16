d49ef8cfe8d281e15af5b8b8855eecc90019f0c1
kalyan7 <kalyan7.boppana@gmail.com>
2016-02-15 16:12:26 -0800

    deleted

--- test/controllers/assignment3_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule KWYE.Assignment3Test do
-  use KWYE.ConnCase
-
-  test "GET /chris", %{conn: conn} do
-    conn = get conn, "/chris"
-    assert html_response(conn, 200) =~ "kalyan hello"
-  end
-end
