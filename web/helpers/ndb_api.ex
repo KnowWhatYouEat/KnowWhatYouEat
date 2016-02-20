c8da3ad6c3a4c249862b5d74951e66e0f93b1cf3
cvcpp <cdvigus@cpp.edu>
2016-02-20 07:02:32 -0800

    Fixed up pipe formatting

--- web/helpers/ndb_api.ex
+++ web/helpers/ndb_api.ex
@@ -16,26 +16,26 @@ defmodule KWYE.Helpers.NDB_API do
 
    defp process_url(url) do
       url
-      |> HTTPoison.get!
-      |> Map.get(:body)
-      |> Poison.decode!
+         |> HTTPoison.get!
+         |> Map.get(:body)
+         |> Poison.decode!
    end
 
 
    def get_food_report(food_id, type \\ :basic) when type in @report_types do
       "#{@ndb_base_url}reports/?ndbno=#{food_id}&type=#{report_type_to_string(type)}&format=json&api_key=#{@api_key}"
-      |> process_url
-      |> Map.get("report")
-      |> Map.get("food")
-      |> Map.get("nutrients")
-      |> Enum.reduce(%{}, fn(nutrient, acc) -> Map.put acc, nutrient["name"], (Map.delete nutrient, "name") end)
+         |> process_url
+         |> Map.get("report")
+         |> Map.get("food")
+         |> Map.get("nutrients")
+         |> Enum.reduce(%{}, fn(nutrient, acc) -> Map.put acc, nutrient["name"], (Map.delete nutrient, "name") end)
    end
 
    def get_search_results(food_name, max \\ 50, offset \\ 0) do
       "#{@ndb_base_url}search/?format=json&q=#{food_name}&sort=r&max=#{max}&offset=#{offset}&api_key=#{@api_key}"
-      |> process_url
-      |> Map.get("list")
-      |> Map.get("item")
+         |> process_url
+         |> Map.get("list")
+         |> Map.get("item")
    end
 
 end
