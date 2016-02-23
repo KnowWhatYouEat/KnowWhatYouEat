8665ca4faaf24f4a5ab83ece3eda17241d364baa
cvcpp <cdvigus@cpp.edu>
2016-02-22 22:21:22 -0800

    Added utility function to flatten a list of maps to a map

---
+++ web/helpers/utility.ex
@@ -0,0 +1,7 @@
+defmodule KWYE.Helpers.Utility do
+
+   def flatten_to_map_by_inner_key(mlist, inner_key) do
+      Enum.reduce(mlist, %{}, fn(e, acc) -> Map.put acc, e[inner_key], (Map.delete e, inner_key) end)
+   end
+
+end
