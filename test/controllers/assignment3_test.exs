fcfdc0bf1f5a47cfc39b487270b536ddea365d78
cvcpp <cdvigus@cpp.edu>
2016-03-03 07:44:29 -0800

    Moved test to correct file so it runs

--- test/controllers/assignment3_test.exs
+++ test/controllers/assignment3_test.exs
@@ -5,4 +5,10 @@ defmodule KWYE.Assignment3Test do
     conn = get conn, "/chris"
     assert html_response(conn, 200) =~ "chris hello"
   end
+
+   test "GET /Ashwag", %{conn: conn} do
+      conn = get conn, "/Ashwag"
+      assert html_response(conn, 200) =~ "ashwagggg hello"
+   end
+
 end
--- test/controllers/assignment3_testAs.exs
+++
@@ -1,8 +0,0 @@
-defmodule KWYE.Assignment3_testAs do
-  use KWYE.ConnCase
-
-  test "GET /Ashwag", %{conn: conn} do
-    conn = get conn, "/Ashwag"
-    assert html_response(conn, 200) =~ "ashwagggg hello"
-  end
-end
