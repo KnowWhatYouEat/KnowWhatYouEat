fe7c1148f6c2b49cdf0627ac8a89b1214809013c
cvcpp <cdvigus@cpp.edu>
2016-03-03 08:06:15 -0800

    Fixed up formatting

--- test/controllers/assignment3_test.exs
+++ test/controllers/assignment3_test.exs
@@ -1,11 +1,14 @@
 defmodule KWYE.Assignment3Test do
   use KWYE.ConnCase
- test "GET /kalyan", %{conn: conn} do
+
+  test "GET /kalyan", %{conn: conn} do
     conn = get conn, "/kalyan"
     assert html_response(conn, 200) =~ "kalyan hello"
-   end
+  end
+
   test "GET /chris", %{conn: conn} do
     conn = get conn, "/chris"
     assert html_response(conn, 200) =~ "chris hello"
-     end
+  end
+
 end
