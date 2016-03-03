9a223da04ffc388486850a149686184ef9769e35
cvcpp <cdvigus@cpp.edu>
2016-03-02 22:12:11 -0800

    Search saves all query parameters

--- web/controllers/nutrition_controller.ex
+++ web/controllers/nutrition_controller.ex
@@ -3,14 +3,15 @@ defmodule KWYE.NutritionController do
 
    alias KWYE.Helpers.NDB_API, as: NDB
 
-   def search(conn, _params) do
-      render conn, "search.html"
+   def search(conn, params) do
+      render conn, "search.html",
+         save_params: params
    end
 
    def options(conn, params) do
       %{"ingredient" => ingredient} = params
       save_params = Map.delete(params, "ingredient")
-
+      
       render conn, "options.html",
          options: NDB.get_search_results(ingredient),
          save_params: save_params,
