d7ebd99d8aab8bcb82d782bead8d5a3adae00f6c
cvcpp <cdvigus@cpp.edu>
2016-03-02 22:11:43 -0800

    Option saves unused query parameters

--- web/controllers/nutrition_controller.ex
+++ web/controllers/nutrition_controller.ex
@@ -7,8 +7,14 @@ defmodule KWYE.NutritionController do
       render conn, "search.html"
    end
 
-   def options(conn, %{"ingredient" => ingredient} = params) do
-      render conn, "options.html", options: NDB.get_search_results(ingredient)
+   def options(conn, params) do
+      %{"ingredient" => ingredient} = params
+      save_params = Map.delete(params, "ingredient")
+
+      render conn, "options.html",
+         options: NDB.get_search_results(ingredient),
+         save_params: save_params,
+         next_id: Enum.count(save_params)
    end
 
 end
