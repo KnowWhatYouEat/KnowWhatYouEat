4f585f9fadbaaad386210247632d4ceac0f344d6
cvcpp <cdvigus@cpp.edu>
2016-03-03 07:43:49 -0800

    Fixed route and expected result

--- test/controllers/assignment3_testAs.exs
+++ test/controllers/assignment3_testAs.exs
@@ -1,8 +1,8 @@
 defmodule KWYE.Assignment3_testAs do
   use KWYE.ConnCase
 
-  test "GET /ashwagg", %{conn: conn} do
-    conn = get conn, "/ashwagg"
-    assert html_response(conn, 200) =~ "ashwag hello"
+  test "GET /Ashwag", %{conn: conn} do
+    conn = get conn, "/Ashwag"
+    assert html_response(conn, 200) =~ "ashwagggg hello"
   end
 end
