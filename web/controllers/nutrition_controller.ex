053f478c5fbf052462afdbe3a6b9522571a4411d
cvcpp <cdvigus@cpp.edu>
2016-03-02 21:34:53 -0800

    Added radio form to options to select correct ingredient

--- web/controllers/nutrition_controller.ex
+++ web/controllers/nutrition_controller.ex
@@ -1,12 +1,14 @@
 defmodule KWYE.NutritionController do
    use KWYE.Web, :controller
 
+   alias KWYE.Helpers.NDB_API, as: NDB
+
    def search(conn, _params) do
       render conn, "search.html"
    end
 
    def options(conn, %{"ingredient" => ingredient} = params) do
-      render conn, "options.html", ingredient: ingredient
+      render conn, "options.html", options: NDB.get_search_results(ingredient)
    end
 
 end
